55a3a6c56e9abe634790187598b05ca3c4c727a4
Songxi <37897076+SongxiYoung@users.noreply.github.com>
2024-07-31 10:38:20 -0500

    Update function_LST(Storage).js

--- downloadData_function-source/function_LST(Storage).js
+++ downloadData_function-source/function_LST(Storage).js
@@ -20,7 +20,7 @@ exports.download = function (message, context) {
 
 /**
  * Function to add "DOY" as bandname
- */ 
+ */
 var addBandname = function(img) {
     var datestring = ee.String(img.get('system:index'));
     var format = 'YYYY_MM_dd';
@@ -28,136 +28,109 @@ var addBandname = function(img) {
     var doy = eedate.getRelative('day', 'year').add(1);
     var year = eedate.get('year');
     var bandname = ee.String(year).cat('_').cat(doy);
-    return img.set('bandname',bandname);
+    return img.set('bandname', bandname);
 };
 
 /**
  * Function to import LST
- */ 
-function stackLST(start,end,region,mask,mode) {
-  // mode: 'Day' or 'Night'
-  var band = 'LST_' + mode + '_1km';
-  var qcband = 'QC_' + mode;
-  var coll;
-  if(start.substring(0,4) == '2001' | start.substring(0,4) == '2002') {
-    coll = 'MODIS/006/MOD11A1';
-  } else {
-    coll = 'MODIS/006/MYD11A1';
-  }
-  var LST = ee.ImageCollection(coll)
-              .filterDate(start,end)
-              .filterBounds(region)
-              .map(function(img) {return img.updateMask(img.select(qcband).eq(0))})
-              .map(function(img) {return img.select(band).float().multiply(0.02).updateMask(mask)})
-              .map(addBandname)
-              .sort('DOY',true);
-  
-  // print(start,end,region,LST);            
-  var LSTstack = comp.stackCollection(LST);
+ */
+function stackLST(start, end, region, mask, mode) {
+    // mode: 'Day' or 'Night'
+    var band = 'LST_' + mode + '_1km';
+    var qcband = 'QC_' + mode;
+    var coll;
+    if (start.substring(0, 4) == '2001' | start.substring(0, 4) == '2002') {
+        coll = 'MODIS/006/MOD11A1';
+    } else {
+        coll = 'MODIS/006/MYD11A1';
+    }
+    var LST = ee.ImageCollection(coll)
+        .filterDate(start, end)
+        .filterBounds(region)
+        .map(function(img) { return img.updateMask(img.select(qcband).eq(0)) })
+        .map(function(img) { return img.select(band).float().multiply(0.02).updateMask(mask) })
+        .map(addBandname)
+        .sort('DOY', true);
 
-  return LSTstack;
+    // print(start,end,region,LST);            
+    var LSTstack = comp.stackCollection(LST);
+
+    return LSTstack;
 }
 
 /**
- * Export
+ * Export to Cloud Storage
  */
 var exportTable = function(table, prefix) {
-  Export.table.toDrive({
-    collection: table.select([".*"], null, false),
-    description: prefix,
-    folder: 'CDL_corn_LST_previous_CDL',  //*********
-    fileNamePrefix: prefix
-  });
-};
+    var params = {
+        collection: table.select([".*"], null, false),
+        description: prefix,
+        bucket: 'bnntraining-bucket',  // bucket name
+        fileNamePrefix: 'input2/' + prefix, // folder + file name
+        fileFormat: 'CSV'  // Specify the file format (e.g., CSV)
+    };
 
+    // Start export to Google Cloud Storage
+    var exportTask = ee.batch.Export.table.toCloudStorage(params);
+    exportTask.start();
+
+    // Check task status after a delay
+    setTimeout(function() {
+        // Get the status after the delay
+        console.info(ee.data.getTaskStatus(exportTask.id));
+    }, 24000); // Adjust the delay as needed
+};
 
 // get only counties with corn and soy
 // var counties = UScounties.filter(ee.Filter.eq('corn_soy',1)); // Question
 var counties = UScounties;
+print(counties);
+Map.addLayer(counties, {}, 'ctn');
 
 var start = '-03-01';  //*********
 var end = '-11-30';   //*********
 
 // loop through each year ***************
-for(var i = 2002; i <= 2003; i++) { //for(var i = 2002; i < 2021; i++) {
+for (var i = 2002; i <= 2003; i++) { //for(var i = 2002; i < 2021; i++) {
 
-  var year = i.toString();
-  var p_year = (i-1).toString();
-  // get crop mask
-  var cropMask;
-  if(i>2006)
-  {
-    //cropMask = ee.Image('USDA/NASS/CDL/'+year + '_01_01').select('LC_Type1').eq(12);
-    var start_day = year + '-01-01'
-    var end_day = year + '-12-31'
-    
-    var dataset = ee.ImageCollection('USDA/NASS/CDL')
-                  .filter(ee.Filter.date(start_day, end_day))
-                  .first();
-    cropMask = dataset.select('cropland').eq(1); // 1- corn, 5 - soybean
+    var year = i.toString();
+    var p_year = (i - 1).toString();
+    // get crop mask
+    var cropMask;
+    if (i > 2006) {
+        //cropMask = ee.Image('USDA/NASS/CDL/'+year + '_01_01').select('LC_Type1').eq(12);
+        var start_day = year + '-01-01'
+        var end_day = year + '-12-31'
 
-    
-  }
-  else{
-    
-    var mcdband = 'MODIS/006/MCD12Q1/' + p_year + '_01_01';
-    cropMask = ee.Image(mcdband).select('LC_Type1').clip(counties).eq(12);
-  }
-  
-  // get LST day stack
-  var LSTday_stack = stackLST(year+start, year+end, counties, cropMask, 'Day');
-  // print(LSTday_stack);
-  // print(LSTday_stack)
-  var meanLSTday = LSTday_stack.reduceRegions(
-    counties, ee.Reducer.mean(), 500);
+        var dataset = ee.ImageCollection('USDA/NASS/CDL')
+            .filter(ee.Filter.date(start_day, end_day))
+            .first();
+        cropMask = dataset.select('cropland').eq(1); // 1- corn, 5 - soybean
 
-  // export
-  // Export the table to Google Drive
 
-      var params = {
-        collection: meanLSTday,
-        description: 'LSTday_daily_mean_'+year,
-        bucket: 'bnntraining-bucket',  //bucket name
-        fileNamePrefix: 'input2/' + 'LSTday_daily_mean_'+year, // folder + file name
-        fileFormat: 'CSV'              // Specify the file format (e.g., CSV)
-      };
+    } else {
 
-      // Start export to Google Drive
-      var exportTask = ee.batch.Export.table.toCloudStorage(params);
-      exportTask.start();
+        var mcdband = 'MODIS/006/MCD12Q1/' + p_year + '_01_01';
+        cropMask = ee.Image(mcdband).select('LC_Type1').clip(counties).eq(12);
+    }
 
-      // Check task status after a delay
-      setTimeout(function () {
-        // Get the status after the delay
-        console.info(ee.data.getTaskStatus(exportTask.id));
-      }, 24000); // Adjust the delay as needed
-  
-  // get LST night stack
-  var LSTnight_stack = stackLST(year+start, year+end, counties, cropMask, 'Night');
-  var meanLSTnight = LSTnight_stack.reduceRegions(
-    counties, ee.Reducer.mean(), 500);
-  
-  
-  // export
-  // Export the table to Google Drive
-      var params = {
-        collection: meanLSTnight,
-        description: 'LSTnight_daily_mean_'+year,
-        bucket: 'bnntraining-bucket',  //bucket name
-        fileNamePrefix: 'input2/' + 'LSTnight_daily_mean_'+year, // folder + file name
-        fileFormat: 'CSV'              // Specify the file format (e.g., CSV)
-      };
+    // get LST day stack
+    var LSTday_stack = stackLST(year + start, year + end, counties, cropMask, 'Day');
+    // print(LSTday_stack);
+    print(LSTday_stack)
+    var meanLSTday = LSTday_stack.reduceRegions(
+        counties, ee.Reducer.mean(), 500);
+    exportTable(meanLSTday, 'LSTday_daily_mean_' + year);
 
-      // Start export to Google Drive
-      var exportTask = ee.batch.Export.table.toCloudStorage(params);
-      exportTask.start();
+    // get LST night stack
+    var LSTnight_stack = stackLST(year + start, year + end, counties, cropMask, 'Night');
+    var meanLSTnight = LSTnight_stack.reduceRegions(
+        counties, ee.Reducer.mean(), 500);
 
-      // Check task status after a delay
-      setTimeout(function () {
-        // Get the status after the delay
-        console.info(ee.data.getTaskStatus(exportTask.id));
-      }, 24000); // Adjust the delay as needed
+    exportTable(meanLSTnight, 'LSTnight_daily_mean_' + year);
+
 }
+
       
     });
   });
